ea82204c667ac5f3f459d3044eae32a8f5b23986
Jonas Silva <89052145+jbs1995@users.noreply.github.com>
2021-12-16 15:24:01 -0300

    Add files via upload

---
+++ consertos.js
@@ -0,0 +1,229 @@
+let consertos = [
+    {
+        aparelhos: 'Asus 10',
+        itensConsertados: [
+            'tela',
+            'bateria',
+            'teclado'
+        ],
+        valores: [
+            60,
+            100,
+            40
+        ],
+        pronto: false
+    },
+ 
+    {
+        aparelhos: 'Samsung A10',
+        itensConsertados: [
+            'tela',
+            'bateria',
+            'teclado'
+        ],
+        valores: [
+            60,
+            100,
+            40
+        ],
+        pronto: false
+    },
+
+    {
+        aparelhos: 'Nokia note',
+        itensConsertados: [
+            'tela',
+            'bateria',
+            'teclado'
+        ],
+        valores: [
+            60,
+            100,
+            40
+        ],
+        pronto: false
+    },
+
+    {
+        aparelhos: 'Motorola',
+        itensConsertados: [
+            'tela',
+            'bateria',
+            'teclado'
+        ],
+        valores: [
+            60,
+            100,
+            40
+        ],
+        pronto: true
+    },
+
+    {
+        aparelhos: 'Samsung A8',
+        itensConsertados: [
+            'tela',
+            'bateria',
+            'teclado'
+        ],
+        valores: [
+            60,
+            100,
+            40
+        ],
+        pronto: false
+    },
+
+    {
+        aparelhos: 'LG',
+        itensConsertados: [
+            'tela',
+            'bateria',
+            'teclado'
+        ],
+        valores: [
+            60,
+            100,
+            40
+        ],
+        pronto: true
+    },
+
+    {
+        aparelhos: 'Samsung',
+        itensConsertados: [
+            'tela',
+            'bateria',
+            'teclado'
+        ],
+        valores: [
+            60,
+            100,
+            40
+        ],
+        pronto: false
+    },
+
+    {
+        aparelhos: 'Samsung galaxy 7',
+        itensConsertados: [
+            'tela',
+            'bateria',
+            'teclado'
+        ],
+        valores: [
+            60,
+            100,
+            40
+        ],
+        pronto: true
+    },
+
+    {
+        aparelhos: 'Samsung Pad',
+        itensConsertados: [
+            'tela',
+            'bateria',
+            'teclado'
+        ],
+        valores: [
+            60,
+            150,
+            40
+        ],
+        pronto: false
+    },
+
+    {
+        aparelhos: 'Iphone 8',
+        itensConsertados: [
+            'tela',
+            'bateria',
+            'teclado'
+        ],
+        valores: [
+            60,
+            300,
+            40
+        ],
+        pronto: true
+    },
+
+    {
+        aparelhos: 'MAC',
+        itensConsertados: [
+            'tela',
+            'bateria',
+            'teclado'
+        ],
+        valores: [
+            900,
+            800,
+            900
+        ],
+        pronto: false
+    },
+
+    {
+        aparelhos: 'Samsung A2',
+        itensConsertados: [
+            'tela',
+            'bateria',
+            'teclado'
+        ],
+        valores: [
+            60,
+            190,
+            80
+        ],
+        pronto: false
+    },
+
+    {
+        aparelhos: 'Nextel 5',
+        itensConsertados: [
+            'tela',
+            'bateria',
+            'teclado'
+        ],
+        valores: [
+            60,
+            100,
+            40,
+            10
+        ],
+        pronto: true
+    },
+
+    {
+        aparelhos: 'Samsung A4',
+        itensConsertados: [
+            'tela',
+            'bateria',
+            'teclado'
+        ],
+        valores: [
+            60,
+            400,
+            40
+        ],
+        pronto: false
+    },
+
+    {
+        aparelhos: 'Redmi 10',
+        itensConsertados: [
+            'tela',
+            'bateria',
+            'teclado'
+        ],
+        valores: [
+            80,
+            380,
+            40
+        ],
+        pronto: true
+    },
+];
+
+console.log(consertos)
